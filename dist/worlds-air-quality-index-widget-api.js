cad772aad762fe6708efdc0d85e68e93223144ea
pawkakol1 <pawel.kakol130@gmail.com>
2024-03-15 00:01:02 +0100

    Cities config parameter supporting; Consoles removed; Comments added

--- dist/worlds-air-quality-index-widget-api.js
+++ dist/worlds-air-quality-index-widget-api.js
@@ -2,99 +2,102 @@
  * Hassio frontend custom worlds-air-quality-index-widget-api
  */
 class WorldsAirQualityIndexWidgetApi extends HTMLElement {
+  /**
+   * Constructor
+   */
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.elementsSet = false;
   }
 
+  /**
+   * Sets configuration of the card
+   * @param {Collection} config 
+   */
   setConfig(config) {
-    //console.log('setConfig:try:loadAqiFeedScript');
-    const root = this.shadowRoot;
-    //this.loadAqiFeedScript(window, 'script', '_aqiFeed');
-    //console.log('setConfig:loaded:loadAqiFeedScript');
-    if (root.lastChild) root.removeChild(root.lastChild);
-
+    const root = this.shadowRoot
+    if (root.lastChild) 
+      root.removeChild(root.lastChild);
     const cardConfig = Object.assign({}, config);
     this._config = cardConfig
   }
 
-/**
- * Script downloaded from https://aqicn.org/faq/2015-07-28/air-quality-widget-new-improved-feed/
- */
-  loadAqiFeedScript(w,t,f) {
+  /**
+   * Script downloaded from https://aqicn.org/faq/2015-07-28/air-quality-widget-new-improved-feed/
+   * adjusted to work with worlds-air-quaility
+   */
+  loadAqiFeedScript() {
     if(window[`scriptLoaded_AqiFeedScript`]){
       return;
     }
-    console.log("loadAqiFeedScript");
-    w[f] = w[f] || function(c,k,n) {
-      console.log("AqiFeed Func1");
-      var s = w[f], k = s['k'] = (s['k'] || (k ? ('&k=' + k) : ''));
-      console.log("AqiFeed Func2");
+    window['_aqiFeed'] = window['_aqiFeed'] || function(c,k,n) {
+      var s = window['_aqiFeed'], k = s['k'] = (s['k'] || (k ? ('&k=' + k) : ''));
       s['c'] = c = (c instanceof Array) ? c : [c];
-      console.log("AqiFeed Func3");
       s['n'] = n = n || 0;
-      console.log("AqiFeed Func4");
-      var L = document.createElement(t);
-      var e = document.getElementsByTagName('body')[0]; //need to be done for last element!!!
-      console.log("AqiFeed Func5");
+      var L = document.createElement('script');
+      var e = document.getElementsByTagName('body')[0];
       L.async = 1;
-      console.log("AqiFeed Func6");
       L.src = 'https://feed.aqicn.org/feed/' + (c[n].city) + '/' + (c[n].lang || '') + '/feed.v1.js?n=' + n + k;
-      console.log(e);
-      console.log(L);
       e.appendChild(L);
     };
     window[`scriptLoaded_AqiFeedScript`] = true;
   }
 
+  /**
+   * 
+   * @param {Collection} hass 
+   */
   set hass(hass) {
     const config = this._config;
     const root = this.shadowRoot;
     const bodyRoot = document.getElementsByTagName('body')[0];
     try{
-      console.log('try!');
       this._hass = hass;
-      console.log(window);
-      console.log(this.content);
-      console.log((!this.content && window[`scriptLoaded_AqiFeedScript`]));
       if(!this.content) {
-        console.log("create elements");
         const card = document.createElement('ha-card');
         this.temporaryContent = document.createElement('div');
         this.temporaryContent.setAttribute("id", "city-aqi-container");
         bodyRoot.appendChild(this.temporaryContent);
-        //root.appendChild(card);
-        console.log(this.shadowRoot.querySelector('#city-aqi-container'));
-        this.loadAqiFeedScript(window, 'script', '_aqiFeed');
-        console.log(document.getElementById('city-aqi-container'));
+        if(!window[`scriptLoaded_AqiFeedScript`])
+          this.loadAqiFeedScript();
         this.elementsSet = true;
-        console.log("elements creating done");
         if(window[`scriptLoaded_AqiFeedScript`]) {
           console.log(config);
           this.content = document.createElement('div');
           this.content.setAttribute("id", "waqi-container");
-          console.log('start!');
           const observerConfig = {childList: true};
           const callback = (mutationList, observer) => {
             for (const mutation of mutationList) {
               if (mutation.type === "childList") {
-                console.log("A child node has been added or removed.");
-                var content = document.getElementById('city-aqi-container').innerHTML;
-                console.log(content);
-                this.content.innerHTML = content;
+                this.content.innerHTML = document.getElementById('city-aqi-container').innerHTML;
                 bodyRoot.removeChild(this.temporaryContent);
-                console.log('child removed!');
                 observer.disconnect();
               }
             }
           };
           const observer = new MutationObserver(callback);
           observer.observe(this.temporaryContent, observerConfig);
-
-          console.log('_aqiFeed!');
-          _aqiFeed({container:"city-aqi-container", city: config.city, lang: config.lang, display: config.display}); 
-          console.log('_aqiFeed done!');
+          if(config.city != undefined || config.cities != undefined) {
+            var parameters = {container:"city-aqi-container"};
+            if(config.lang != undefined)
+              parameters["lang"] = config.lang;
+            if(config.display != undefined)
+              parameters["display"] = config.display;
+            
+            if(config.cities != undefined) {
+              for(var i = 0; i < config.cities.lenght; i++) {
+                parameters["city"] = config.cities[i];
+                _aqiFeed(parameters); 
+              }
+            }
+            else {
+              parameters["city"] = config.city;
+              _aqiFeed(parameters); 
+            }
+          }
+          else 
+            throw new Error('The minimum parameter is singe "city" or "cities" array');
           card.appendChild(this.content);
           root.appendChild(card);
         }
@@ -103,7 +106,7 @@ class WorldsAirQualityIndexWidgetApi extends HTMLElement {
       console.trace();
       console.log(err);
       console.log('waiting for AQI feed script load');
-      this.loadAqiFeedScript(window, 'script', '_aqiFeed');
+      this.loadAqiFeedScript();
     }
   }
 
